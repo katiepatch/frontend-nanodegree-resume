a310d21c598cbd6623b908e0b218a3ba162dbb91
cocoswank <patchkatie@gmail.com>
2015-04-05 23:44:38 -0400

    Change out header text in js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,21 @@
-$("#main").append("Katie");
+
+
+// var firstName = "Katie";
+// console.log(firstName);
+
+// var age = 32;
+
+// var awesomeThoughts = "I am Katie and I am Awesome!";
+// console.log(awesomeThoughts);
+
+// var funThoughts = awesomeThoughts.replace("Awesome", "Fun");
+// console.log(funThoughts);
+
+// $("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Katie Patch");
+$("#header").append(formattedName);
+
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$("#header").append(formattedRole);
